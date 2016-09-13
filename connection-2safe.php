25e5c5686b30768a4c0240b7498f3dca36437711
Admin <Admin@Admins-MacBook.gci.net>
2016-09-12 17:34:24 -0800

    Initialized repository

---
+++ connection-2safe.php
@@ -0,0 +1,13 @@
+<?php
+
+
+  $link = mysqli_connect("#######", "#######", "#######", "#######");
+     
+
+      if (mysqli_connect_error()) {
+
+        die ("Database connection error");
+
+      }
+
+?>
---
+++ header-2safe.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+   <style type="text/css">
+  
+     .container {
+          
+       text-align:center;
+       width: 400px;
+      
+      }
+     
+     #homePageContainer {
+     
+        margin-top: 150px;
+     
+     }
+ 
+  
+     html { 
+       
+       background: url(background.jpg) no-repeat center center fixed; 
+       -webkit-background-size: cover;
+       -moz-background-size: cover;
+       -o-background-size: cover;
+       background-size: cover;
+     }
+      
+     body {
+        
+        background: none;
+        color:#FFF;
+          
+      }
+      
+      #logInForm {
+          
+          display:none;
+      }
+      
+      .toggleForms {
+          
+          font-weight: bold;
+      }
+      
+      #containerLoggedInPage {
+      
+        margin-top: 60px;
+         
+      }
+      
+      .navbar {
+      
+         opacity: 0.65;
+         filter: alpha(opactity=65);      
+      }
+      
+      #diary {
+        height:250px;
+      
+        color: dimgray;
+        font-weight: ;
+        opacity: 0.6;
+        filter: alpha(opactity=60);   
+      
+      }
+      
+      #brandTitle {
+        
+        font-size: 1.5em;
+        color: #424242;
+        opacity: 0.85;
+        filter: alpha(opactity=85; 
+      
+      }
+      
+      #leadLine {
+        
+        font-style: italic; 
+        color: #FFF;
+      
+      }
+      
+     
+ </style>
+    
+ </head>
+ <body>
